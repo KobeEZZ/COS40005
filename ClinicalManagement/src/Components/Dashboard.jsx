ce97c7d39568dd4f1123adc999f68f9977c194b2
L1ttt <longth0903@gmail.com>
2025-09-30 21:44:14 +0700

    .

---
+++ ClinicalManagement/src/Components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import './Dashboard.css'
+import React ,{useState} from 'react'
+function Dashboard() {
+  
+  return (
+    <>
+      This is Dashboard
+    </>
+  )
+}
+
+export default Dashboard
--- ClinicalManagement/src/main.jsx
+++ ClinicalManagement/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginSignup from './Components/LoginSignup.jsx'
+import Dashboard from './Components/Dashboard.jsx'
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <LoginSignup /> },
+  { path: "/dashboard", element: <Dashboard /> }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
